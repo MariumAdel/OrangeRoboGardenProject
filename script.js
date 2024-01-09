4ea9ef37c71e035380a659b90263628da6f4ceec
Marium <38459047+MariumAdel@users.noreply.github.com>
2024-01-10 01:50:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+
+        $(document).ready(function(){
+            $('.like-btn').click(function(e){
+                e.preventDefault();
+               
+                if($(this).hasClass('like'))
+                {
+                    $(this).removeClass('like');
+                    $(this).addClass('dislike');
+                    $(this).html('Dislike');
+                }
+                else
+                {
+                    $(this).removeClass('dislike');
+                    $(this).addClass('like');
+                    $(this).html('Like');
+                }
+            });
+
+            
+    $('#submit_form').submit(function(e){
+        e.preventDefault();
+
+        if(!$('input[name=name]').val()){
+            alert('Name field is required.');
+            return;
+        }
+
+        if(!$('input[name=email]').val()){
+            alert('Email field is required.');
+            return;
+        }
+
+        if(!$('input[name=phone]').val()){
+            alert('Phone field is required.');
+            return;
+        }
+
+        if(!$('textarea[name=message]').val()){
+            alert('Message field is required.');
+            return;
+        }
+
+        $('#success_message').show();
+    });
+    
+        });
+
+
+   
